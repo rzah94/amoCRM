d833ec96195b35d746d54ac44ddfb35f2fb0b9ea
Roman <rzah94@gmail.com>
2021-08-31 23:56:47 +0300

    first

--- AmoIntegration.php
+++ AmoIntegration.php
@@ -13,3 +13,5 @@ $redirect_uri =  'https://rzah-project.ru/newAmoIntegration/AmoIntegration.php';
 $client_id = '4a7b14d4-ceea-4c8f-8ad1-eccd55f8bad6';
 $client_secret = '5N0GXM0zmlhVRL7zQGAT6jAfyCaFqf6TlxgsvUBnTMNwuvGkfL5vdgXKpGnqBxEO';
 
+
+
